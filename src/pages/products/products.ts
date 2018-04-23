c7808e4cbc1c7bf09b01a623cd9c3dcdf998d0fc
Chai Wei Jun <wei4.chai@live.uwe.ac.uk>
2018-04-23 22:10:26 +0100

    hide tab - productpage

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-products',
-  templateUrl: 'products.html',
+    selector: 'page-products',
+    templateUrl: 'products.html',
 })
 export class ProductsPage {
+    tabBarElement: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.tabBarElement = document.querySelector('.tabbar.show-tabbar');
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ProductsPage');
+    }
+
+    // hide the tab-bar when entering
+    ionViewWillEnter() {
+        this.tabBarElement.style.display = "none";
+    }
+
+    // unhide tab-bar when leaving
+    ionViewWillLeave() {
+        this.tabBarElement.style.display = "flex";
+    }
 
 }
